2ba1cec77da8b063a4a65a2a394fbd8d9eb596e9
leeluna0476 <leeluna0476@daum.net>
2024-04-02 17:44:03 +0900

    add constant

--- 02/ex02/Fixed.cpp
+++ 02/ex02/Fixed.cpp
@@ -1,5 +1,4 @@
 #include "Fixed.hpp"
-#include <iostream>
 #include <cmath>
 
 Fixed::Fixed(void) : value(0)
@@ -53,58 +52,52 @@ float	Fixed::toFloat(void) const
 	return (float)value / (1 << bit);
 }
 
-bool	Fixed::operator >(const Fixed &fixed)
+bool	Fixed::operator >(const Fixed &fixed) const
 {
 	return value > fixed.getRawBits();
 }
 
-bool	Fixed::operator <(const Fixed &fixed)
+bool	Fixed::operator <(const Fixed &fixed) const
 {
 	return value < fixed.getRawBits();
 }
 
-bool	Fixed::operator >=(const Fixed &fixed)
+bool	Fixed::operator >=(const Fixed &fixed) const
 {
 	return value >= fixed.getRawBits();
 }
 
-bool	Fixed::operator <=(const Fixed &fixed)
+bool	Fixed::operator <=(const Fixed &fixed) const
 {
 	return value <= fixed.getRawBits();
 }
 
-bool	Fixed::operator ==(const Fixed &fixed)
+bool	Fixed::operator ==(const Fixed &fixed) const
 {
 	return value == fixed.getRawBits();
 }
 
-bool	Fixed::operator !=(const Fixed &fixed)
+bool	Fixed::operator !=(const Fixed &fixed) const
 {
 	return value != fixed.getRawBits();
 }
 
-std::ostream	&operator <<(std::ostream &os, const Fixed &fixed)
-{
-	os << fixed.toFloat();
-	return os;
-}
-
-float	Fixed::operator +(const Fixed &fixed)
+float	Fixed::operator +(const Fixed &fixed) const
 {
 	return toFloat() + fixed.toFloat();
 }
 
-float	Fixed::operator -(const Fixed &fixed)
+float	Fixed::operator -(const Fixed &fixed) const
 {
 	return toFloat() - fixed.toFloat();
 }
 
-float	Fixed::operator *(const Fixed &fixed)
+float	Fixed::operator *(const Fixed &fixed) const
 {
 	return toFloat() * fixed.toFloat();
 }
 
-float	Fixed::operator /(const Fixed &fixed)
+float	Fixed::operator /(const Fixed &fixed) const
 {
 	float	f_rawbit;
 
@@ -140,24 +133,26 @@ Fixed	Fixed::operator --(int)
 
 Fixed	&Fixed::min(Fixed &fixed1, Fixed &fixed2)
 {
-	std::cout << "min: ";
 	return fixed1 < fixed2 ? fixed1 : fixed2;
 }
 
 Fixed	&Fixed::max(Fixed &fixed1, Fixed &fixed2)
 {
-	std::cout << "max: ";
 	return fixed1 > fixed2 ? fixed1 : fixed2;
 }
 
 const Fixed	&Fixed::min(const Fixed &fixed1, const Fixed &fixed2)
 {
-	std::cout << "const min: ";
 	return fixed1.getRawBits() < fixed2.getRawBits() ? fixed1 : fixed2;
 }
 
 const Fixed	&Fixed::max(const Fixed &fixed1, const Fixed &fixed2)
 {
-	std::cout << "const max: ";
 	return fixed1.getRawBits() > fixed2.getRawBits() ? fixed1 : fixed2;
+}
+
+std::ostream	&operator <<(std::ostream &os, const Fixed &fixed)
+{
+	os << fixed.toFloat();
+	return os;
 }
--- 02/ex02/Fixed.hpp
+++ 02/ex02/Fixed.hpp
@@ -12,8 +12,8 @@ class	Fixed
 	public:
 		Fixed(void);
 		Fixed(const Fixed &fixed);
-		Fixed	&operator =(const Fixed &fixed);
 		~Fixed(void);
+		Fixed	&operator =(const Fixed &fixed);
 
 		Fixed(const int &_int);
 		Fixed(const float &_float);
@@ -24,27 +24,27 @@ class	Fixed
 		int		toInt(void) const;
 		float	toFloat(void) const;
 
-		bool	operator >(const Fixed &fixed);
-		bool	operator <(const Fixed &fixed);
-		bool	operator >=(const Fixed &fixed);
-		bool	operator <=(const Fixed &fixed);
-		bool	operator ==(const Fixed &fixed);
-		bool	operator !=(const Fixed &fixed);
+		bool	operator >(const Fixed &fixed) const;
+		bool	operator <(const Fixed &fixed) const;
+		bool	operator >=(const Fixed &fixed) const;
+		bool	operator <=(const Fixed &fixed) const;
+		bool	operator ==(const Fixed &fixed) const;
+		bool	operator !=(const Fixed &fixed) const;
 
-		float	operator +(const Fixed &fixed);
-		float	operator -(const Fixed &fixed);
-		float	operator *(const Fixed &fixed);
-		float	operator /(const Fixed &fixed);
+		float	operator +(const Fixed &fixed) const;
+		float	operator -(const Fixed &fixed) const;
+		float	operator *(const Fixed &fixed) const;
+		float	operator /(const Fixed &fixed) const;
 
 		Fixed	&operator ++();
 		Fixed	&operator --();
 		Fixed	operator ++(int);
 		Fixed	operator --(int);
 
-		static	Fixed	&min(Fixed &fixed1, Fixed &fixed2);
-		static	Fixed	&max(Fixed &fixed1, Fixed &fixed2);
-		static	const Fixed	&min(const Fixed &fixed1, const Fixed &fixed2);
-		static	const Fixed	&max(const Fixed &fixed1, const Fixed &fixed2);
+		static	Fixed		&min(Fixed &fixed1, Fixed &fixed2);
+		static	Fixed		&max(Fixed &fixed1, Fixed &fixed2);
+		static const Fixed	&min(const Fixed &fixed1, const Fixed &fixed2);
+		static const Fixed	&max(const Fixed &fixed1, const Fixed &fixed2);
 };
 
 std::ostream	&operator <<(std::ostream &os, const Fixed &fixed);
