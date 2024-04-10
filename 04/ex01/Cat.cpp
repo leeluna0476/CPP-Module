3b06658057042025deed3e24f96dd6c11c27e3d4
leeluna0476 <leeluna0476@daum.net>
2024-04-10 15:26:38 +0900

    update

--- 04/ex01/Cat.cpp
+++ 04/ex01/Cat.cpp
@@ -10,18 +10,13 @@ Cat::Cat(void) : brain(new Brain())
 Cat::Cat(const Cat& other) : Animal(other)
 {
 	std::cout << "Cat copy constructor called" << std::endl;
-	if (this != &other)
-		brain = new Brain(*(other.brain));
+	brain = new Brain(*(other.brain));
 }
 
 Cat& Cat::operator=(const Cat& other)
 {
 	std::cout << "Cat copy assignment operator called" << std::endl;
-	if (this != &other)
-	{
-		this->type = other.type;
-		*brain = *(other.brain);
-	}
+	Animal::operator=(other);
 	return *this;
 }
 
--- 04/ex01/Dog.cpp
+++ 04/ex01/Dog.cpp
@@ -10,18 +10,13 @@ Dog::Dog(void) : brain(new Brain())
 Dog::Dog(const Dog& other) : Animal(other)
 {
 	std::cout << "Dog copy constructor called" << std::endl;
-	if (this != &other)
-		brain = new Brain(*(other.brain));
+	brain = new Brain(*(other.brain));
 }
 
 Dog& Dog::operator=(const Dog& other)
 {
 	std::cout << "Dog copy assignment operator called" << std::endl;
-	if (this != &other)
-	{
-		this->type = other.type;
-		*brain = *(other.brain);
-	}
+	Animal::operator=(other);
 	return *this;
 }
 
