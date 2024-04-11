e2a060dda7fa6996eec97202cdd016704c7f6515
leeluna0476 <leeluna0476@daum.net>
2024-04-11 21:47:55 +0900

    initialize main

--- 05/ex01/main.cpp
+++ 05/ex01/main.cpp
@@ -5,21 +5,6 @@ void	terminateProgram(void);
 
 int	main(void)
 {
-	std::set_terminate(terminateProgram);
-
-	try
-	{
-		Bureaucrat	a("wocheon", 2);
-		Bureaucrat	b("jasong", 160);
-		--a;
-		++b;
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-		return 1;
-	}
-
 	return 0;
 }
 
