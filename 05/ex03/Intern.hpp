661aaf9ea474eb41fed81381e40c4f1b88f23c4d
Seojin Lee <seojilee@c1r2s2.42seoul.kr>
2024-04-14 18:05:57 +0900

    remove every content of Intern: study more about exception

--- 05/ex03/Intern.hpp
+++ 05/ex03/Intern.hpp
@@ -11,20 +11,6 @@ class	Intern
 		Intern(const Intern& other);
 		Intern&	operator=(const Intern& other);
 		~Intern(void);
-
-		AForm*	makeForm(const std::string& form_name, const std::string& target) const;
-
-		class	NoSuchForm : public std::exception
-		{
-			private:
-				std::string	form_name;
-			public:
-				NoSuchForm(const std::string& _form_name);
-				~NoSuchForm(void) _NOEXCEPT;
-
-				const std::string&	getFormName(void) const;
-				const char*	what(void) const throw();
-		};
 };
 
 #endif
--- 05/ex03/main.cpp
+++ 05/ex03/main.cpp
@@ -8,30 +8,6 @@ void	terminateProgram(void);
 int	main(void)
 {
 	std::set_terminate(terminateProgram);
-
-	Bureaucrat	jisokang("jisokang", 1);
-
-	Intern	seojilee;
-	std::string	form1 = "RobotomyRequestForm";
-	std::string	form2 = "SaveSeoileeForm";
-
-	try
-	{
-		AForm*	d = seojilee.makeForm(form1, "member");
-		AForm*	e = seojilee.makeForm(form2, "nooo");
-
-		jisokang.signForm(*d);
-		jisokang.executeForm(*d);
-		jisokang.signForm(*e);
-		jisokang.executeForm(*e);
-
-		delete d;
-		delete e;
-	}
-	catch (const Intern::NoSuchForm& e)
-	{
-		std::cout << e.what() << ": The intern couldn't find [" << e.getFormName() << "]" << std::endl;
-	}
 }
 
 void	terminateProgram(void)
