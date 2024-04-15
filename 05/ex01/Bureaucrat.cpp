3e774ddf2b91cbf14fd1e78ef34f33920b7d8368
LEE LUNA <118426492+leeluna0476@users.noreply.github.com>
2024-04-15 22:22:05 +0900

    Remove unnecessary return statement

--- 05/ex01/Bureaucrat.cpp
+++ 05/ex01/Bureaucrat.cpp
@@ -73,7 +73,6 @@ void	Bureaucrat::signForm(Form& form)
 	catch (const std::exception& e)
 	{
 		std::cerr << e.what() << ": [" << name << "] couldn't sign the form [" << form.getName() << "]" << std::endl;
-		return ;
 	}
 }
 
