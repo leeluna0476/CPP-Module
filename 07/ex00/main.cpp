4ce402b6f479393289eb3b52f516394b843588bb
seojilee <seojilee@student.42seoul.kr>
2024-12-11 20:30:05 +0900

    add a template for pointer

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -5,21 +5,26 @@ int	main(void)
 {
 	int a = 2;
 	int b = 3;
+
 	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
 
 	::swap(&a, &b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 
 	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	std::cout << "min(&a, &b) = " << ::min(&a, &b) << std::endl;
+	std::cout << "a = " << &a << std::endl;
+	std::cout << "b = " << &b << std::endl;
 	return 0;
 }
--- 07/ex00/whatever.hpp
+++ 07/ex00/whatever.hpp
@@ -27,9 +27,20 @@ T&	min(T& a, T& b)
 	return a < b ? a : b;
 }
 
+template <typename T>
+T*	min(T* a, T* b)
+{
+	return a < b ? a : b;
+}
 
 template <typename T>
 T&	max(T& a, T& b)
+{
+	return a > b ? a : b;
+}
+
+template <typename T>
+T*	max(T* a, T* b)
 {
 	return a > b ? a : b;
 }
