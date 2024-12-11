fd59a0a8f0e3a5655731113106971f3d0988e847
seojilee <seojilee@student.42seoul.kr>
2024-12-11 16:16:59 +0900

    overload swap for ptr

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -5,7 +5,7 @@ int	main(void)
 {
 	int a = 2;
 	int b = 3;
-	::swap( a, b );
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
@@ -15,5 +15,11 @@ int	main(void)
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	::swap(&a, &b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
 	return 0;
 }
--- 07/ex00/whatever.hpp
+++ 07/ex00/whatever.hpp
@@ -12,10 +12,21 @@ void	swap(T& a, T& b)
 }
 
 template <typename T>
+void	swap(T* a, T* b)
+{
+	T	t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+template <typename T>
 T&	min(T& a, T& b)
 {
 	return a < b ? a : b;
 }
+
 
 template <typename T>
 T&	max(T& a, T& b)
