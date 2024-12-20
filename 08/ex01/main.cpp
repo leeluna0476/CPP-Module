6a8b4a856453bd66e31067c3766a2fb3e0051ee1
seojilee <seojilee@student.42seoul.kr>
2024-12-20 13:38:02 +0900

    add the test code fo the subject

--- 08/ex01/main.cpp
+++ 08/ex01/main.cpp
@@ -5,6 +5,7 @@
 int	main(void)
 {
     Span    s(10);
+    Span    sp = Span(5);
 
     try
     {
@@ -16,11 +17,22 @@ int	main(void)
 
         std::cout << s.shortestSpan() << std::endl;
         std::cout << s.longestSpan() << std::endl;
+
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
     }
     catch (const std::exception &e)
     {
         std::cout << e.what() << std::endl;
     }
 
-	return 0;
+    return 0;
+
+    return 0;
 }
