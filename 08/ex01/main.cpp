ca4a093f2f52f850dd4a33d49e4c651db38bf48c
seojilee <seojilee@student.42seoul.kr>
2024-12-26 17:29:05 +0900

    add one more testcase int min and int max

--- 08/ex01/main.cpp
+++ 08/ex01/main.cpp
@@ -16,36 +16,22 @@ int	main(void)
         s.addNumber(4);
         s.addNumber(-1);
         s.addNumber(2147483647);
+        s.addNumber(-2147483648);
 
         s.printElements();
 
         std::cout << s.shortestSpan() << std::endl;
         std::cout << s.longestSpan() << std::endl;
 
-//        sp.addNumber(6);
-//        sp.addNumber(3);
-//        sp.addNumber(17);
-//        sp.addNumber(9);
-//        sp.addNumber(11);
-//
-//        std::cout << sp.shortestSpan() << std::endl;
-//        std::cout << sp.longestSpan() << std::endl;
-
+        const Span  cs = s;
         std::vector<int>    v;
         v.push_back(6);
         v.push_back(3);
-        v.push_back(17);
 
         s.addRange(v.begin(), v.end());
         
         s.printElements();
 
-//        std::vector<char>    v;
-//        v.push_back('a');
-//        v.push_back('b');
-//        v.push_back('c');
-//        s.addRange(v.begin(), v.end());
-//        std::cout << s.longestSpan() << std::endl;
     }
     catch (const std::exception &e)
     {
