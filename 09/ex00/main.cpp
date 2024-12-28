16e4396c9772c7323fe77ea0bd58b662d756fbe0
seojilee <seojilee@student.42seoul.kr>
2024-12-28 16:21:16 +0900

    read input.txt file

--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -1,18 +1,42 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <fstream>
 
-int	main(void)
+int	main(int ac, char **av)
 {
+    if (ac != 2)
+    {
+        return 0;
+    }
+
     BitcoinExchange btc;
+    std::ifstream   infile;
 
     try
     {
-        btc.isValidHeader("date|value");
-        btc.addDataToList(btc.isValidData("2024-12-31 | 333"));
+        infile.open(av[1]);
+        std::string line;
+        std::getline(infile, line);
+        btc.isValidHeader(line);
+
+        while (!std::getline(infile, line).eof())
+        {
+            if (!line.size())
+            {
+                continue;
+            }
+
+            btc.addDataToList(btc.isValidData(line));
+            std::cout << line << std::endl;
+        }
     }
     catch (const Error &e)
     {
         std::cout << "Error: " << e.what() << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
     }
 
 	return 0;
